e840a43d43b744dc03114884a53fe124dd8b96e7
gui3 <gui.silvent@gmail.com>
2022-08-27 15:42:20 +0200

    bugfix call to non ready api

--- client/src/components/SongBrowser.jsx
+++ client/src/components/SongBrowser.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react"
 
 
-export default function Song (props) {
+export default function SongBrowser (props) {
     // in progress
     const [songList, setSongList] = useState({})
 
+    /*
     useEffect(async _ => {
         const songData = await fetch("/api/songs")
         
     })
+    */
+
+    return (
+        <p>Song Browser</p>
+    )
 }
